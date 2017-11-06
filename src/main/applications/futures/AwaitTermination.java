8167903922c802b5acc5daa37ff3925544629d07
Kyle Kroboth <kdk2002@wildcats.unh.edu>
2017-11-06 00:40:49 +0000

    New latex Makefile and directory

---
+++ src/main/applications/futures/AwaitTermination.java
@@ -0,0 +1,13 @@
+package applications.futures;
+
+public class AwaitTermination {
+
+  void start() {
+
+  }
+
+  public static void main(String[] args) {
+    AwaitTermination app = new AwaitTermination();
+    app.start();
+  }
+}
